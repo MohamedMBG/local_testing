ce591579991e32005a0704743d9468ac9e8301f9
MohamedMBG <mohamedbaghdad.me@gmail.com>
2025-12-06 18:36:20 +0100

    collectible completed ( working on the logic if an item got already collected or not

--- src/main/java/game/systems/Collectible.java
+++ src/main/java/game/systems/Collectible.java
@@ -2,8 +2,47 @@ package game.systems;
 
 public class Collectible {
 
-    private double x,y;
-    private double height, width;
-    private boolean collected;
+    protected double x,y;
+    protected double height, width;
+    protected boolean collected;
 
+    public Collectible(double x, double y, double height, double width, boolean collected) {
+        this.x = x;
+        this.y = y;
+        this.height = height;
+        this.width = width;
+        this.collected = collected;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public boolean isCollected() {
+        return collected;
+    }
+
+    public boolean tryCollect(double playerX, double playerY, double playerWidth, double playerHeight) {
+        if (!collected &&
+            playerX < x + width &&
+            playerX + playerWidth > x &&
+            playerY < y + height &&
+            playerY + playerHeight > y) {
+            collected = true;
+            return true;
+        }
+        return false;
+    }
 }
