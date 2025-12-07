51dea0554bf444c3ee8086af17a3197857271897
MohamedMBG <mohamedbaghdad.me@gmail.com>
2025-12-07 21:29:18 +0100

    v1 of UIManager

--- src/main/java/game/systems/UIManager.java
+++ src/main/java/game/systems/UIManager.java
@@ -1,4 +1,18 @@
 package game.systems;
 
+import javafx.scene.canvas.GraphicsContext;
+
 public class UIManager {
+    private double x, y;
+
+    public UIManager(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void render(GraphicsContext gc, int coinCount){
+        // Rendering logic for UI elements
+        gc.fillText("Coins: " + coinCount, x, y);
+
+    }
 }
